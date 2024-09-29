c0e622b21d40a174d9da5d3dc09c942c9f9f8327
AshenLad <77716612+AshenLad@users.noreply.github.com>
2024-09-29 20:09:18 +0800

    Add files via upload
    add dataset_for_pytorch.py

---
+++ dataset_for_pytorch.py
@@ -0,0 +1,46 @@
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from pypcd4 import PointCloud
+
+
+class MyPointDataset(Dataset):
+    def __init__(self, root_dir, pointset='train'):
+        self.root_dir = root_dir
+        self.pointset = pointset
+        #数据路径
+        self.data_path = os.path.join(root_dir, pointset)
+        self.data_list = os.listdir(self.data_path)
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        point_name = self.data_list[index]
+        path = os.path.join(self.data_path, point_name)
+        point = PointCloud.from_path(path)
+        point = point.numpy()
+
+        label = point[:, -1]
+        point = np.delete(point, (3, 4), axis=1)
+
+        return point, label
+
+def collate_fn(data):
+    data2stack = np.stack([item[0] for item in data]).astype(np.float32)
+    label2stack = np.stack([item[1] for item in data]).astype(int)
+
+    return data2stack, label2stack
+
+if __name__ == '__main__':
+    dataset = MyPointDataset(root_dir='./point_file', pointset='train')
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+    # dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    for i,j in enumerate(dataloader):
+        point, label = j
+        print(point.shape)
+        print(label.shape)
+
+
